0ffbb33eda93469c29adc2dc2292595b3e57f634
Pavan Singh <singhpavan78@gmail.com>
2018-07-25 18:28:39 +0200

    Initial Commit

---
+++ Synthesism/Assets/Scripts/CarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	// Use this for initialization
+	private CarMovement car; 
+
+	void Start () {
+		car = GetComponent<CarMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Horizontal")){
+			car.MoveHorizontal();
+		}
+		else if(Input.GetButtonDown("Vertical")){
+			car.MoveVertical();
+		}
+	}
+
+}
---
+++ Synthesism/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement: MonoBehaviour {
+
+	public float speed;
+	private Rigidbody car;
+	
+	// Use this for initialization
+	void Start () {
+		car = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MoveHorizontal() {
+		car.AddForce(transform.right * speed * Time.deltaTime * GetAxisMovement(Input.GetAxis("Horizontal")), ForceMode.Acceleration);
+	}
+
+	public void MoveVertical() {
+		car.AddForce(transform.forward * speed * Time.deltaTime * GetAxisMovement(Input.GetAxis("Vertical")), ForceMode.Acceleration);
+	}
+
+	int GetAxisMovement(float axis) {
+		return axis>0?1:-1;
+	}
+}
