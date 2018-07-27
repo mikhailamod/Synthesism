bd3dc9cb896683035089da0c6007da68e198ac6d
BrandonGower-Winter <brandongowerwinter@gmail.com>
2018-07-27 08:56:13 +0200

    Basic Singleton Skeleton.

---
+++ Synthesism/Assets/Scripts/Managers/MonoSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generic Singleton Implementation for a Monobehavior
+/// </summary>
+/// <typeparam name="T">Type of the Component</typeparam>
+public class MonoSingleton<T> : MonoBehaviour
+{
+
+	private static T _instance;
+
+    public static T instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                
+            }
+            //Add some component creation here
+            //Dont destroy on load
+            return _instance;
+        }
+        private set { }
+    }
+
+	
+}
