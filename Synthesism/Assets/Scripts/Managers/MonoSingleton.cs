a5e8ee924b1608ffb03de473feb58f84a2f34747
Brandon-Gower-Winter <bgwsa101@gmail.com>
2018-07-29 09:34:23 +0200

    Implemented generic singleton pattern to be used by game managers. Also added test class to view the Singleton in a game scence.

--- Synthesism/Assets/Scripts/Managers/MonoSingleton.cs
+++ Synthesism/Assets/Scripts/Managers/MonoSingleton.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ using UnityEngine;
 /// Generic Singleton Implementation for a Monobehavior
 /// </summary>
 /// <typeparam name="T">Type of the Component</typeparam>
-public class MonoSingleton<T> : MonoBehaviour
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
 
 	private static T _instance;
@@ -17,13 +18,40 @@ public class MonoSingleton<T> : MonoBehaviour
         {
             if(_instance == null)
             {
-                
+                _instance = GameObject.FindObjectOfType<T>(); //Look for component in scene
+
+                if (_instance == null) //If there is no componenet in the scene create one
+                {
+                    Debug.Log("No " + typeof(T).Name + " exists in the scene. Creating one...");
+                    createNewSingleton();
+                }
             }
-            //Add some component creation here
-            //Dont destroy on load
             return _instance;
         }
         private set { }
+    }
+
+    private static void createNewSingleton()
+    {
+        GameObject go = new GameObject(typeof(T).Name + "Manager");
+        _instance = go.AddComponent<T>();
+    }
+
+    public virtual void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this as T;
+
+            //This will make the GameObject that the manager is attached to persist across scenes.
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //Destroy the Manager because one already exists
+            Debug.LogWarning("Attempt to create another " + typeof(T).Name + " was made. Destroyed it instead.");
+            Destroy(gameObject);
+        }
     }
 
 	
