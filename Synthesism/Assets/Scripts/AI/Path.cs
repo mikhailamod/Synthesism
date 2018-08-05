6bd68ae24366680dce4402d055b4306226526342
Mikhail Amod <mickeyamod@gmail.com>
2018-08-05 20:25:35 +0200

    WIP: Added basic AI movement. Updated CarMovement

---
+++ Synthesism/Assets/Scripts/AI/Path.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+    public Color lineColor;
+    public bool showLines = true;
+    public float pointRadius = 0.3f;
+
+    private List<Transform> nodes = new List<Transform>();
+
+    private void OnDrawGizmosSelected()
+    {
+        if(showLines)
+        {
+            Gizmos.color = lineColor;
+
+            Transform[] transforms = GetComponentsInChildren<Transform>();
+            nodes = new List<Transform>();
+
+            for (int i = 0; i < transforms.Length; i++)
+            {
+                if (transforms[i] != transform)
+                {
+                    nodes.Add(transforms[i]);
+                }
+            }
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Vector3 currentNodePosition = nodes[i].position;
+                Vector3 previousNodePosition = Vector3.zero;
+                if (i == 0 && nodes.Count > 1)
+                {
+                    previousNodePosition = nodes[nodes.Count - 1].position;
+                }
+                else
+                {
+                    previousNodePosition = nodes[i - 1].position;
+                }
+
+                Gizmos.DrawLine(previousNodePosition, currentNodePosition);
+                Gizmos.DrawWireSphere(currentNodePosition, pointRadius);
+            }
+        }
+    }
+}
--- Synthesism/Assets/Scripts/Car Dynamics/AICarController.cs
+++ Synthesism/Assets/Scripts/Car Dynamics/AICarController.cs
@@ -2,15 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AICarController : MonoBehaviour {
+public class AICarController : MonoBehaviour, ICarController {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Transform path;
+
+    [Header("Movement Properties")]
+    public CarMovement carMovement;
+
+    private List<Transform> nodes;
+    private int currentNode;
+    public float minNodeDistance = 0.5f;
+    public float currentSpeed;
+    public float maxSpeed = 100f;
+ 
+    // Use this for initialization
+    void Start () {
+        currentNode = 0;
+        currentSpeed = 0f;
+        initializePath(path);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        MoveVehicle();
+        UpdateWaypoint();
+    }
+
+    public void MoveVehicle()
+    {
+        float steerAmount = SteerCar();
+        float driveAmount = Drive();
+        carMovement.MoveHorizontal(steerAmount);
+        carMovement.MoveVertical(driveAmount);
+        carMovement.RotateWheels();
+    }
+
+    private void UpdateWaypoint()
+    {
+        float distance = Vector3.Distance(transform.position, nodes[currentNode].position);
+        if (distance < minNodeDistance)
+        {
+            if(currentNode == nodes.Count-1)
+            {
+                currentNode = 0;
+            }
+            else
+            {
+                currentNode++;
+            }
+        }
+    }
+
+    private float SteerCar()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        relativeVector = relativeVector/relativeVector.magnitude;//Vector3.Normalize return positive number
+        return relativeVector.x;
+    }
+
+    private float Drive()
+    {
+        float amount;
+        currentSpeed = carMovement.GetSpeed();
+        if(currentSpeed < maxSpeed)
+        {
+            amount = 1.0f;
+        } else {
+            amount = 0.0f;
+        }
+        return amount;
+    }
+
+    private void initializePath(Transform path)
+    {
+        Transform[] transforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < transforms.Length; i++)
+        {
+            if (transforms[i] != path.transform)
+            {
+                nodes.Add(transforms[i]);
+            }
+        }
+    }//end method
+
+    public float getCurrentSpeed()
+    {
+        return currentSpeed;
+    }
+
+    public float getRpm()
+    {
+        return carMovement.GetRpm();
+    }
 }
