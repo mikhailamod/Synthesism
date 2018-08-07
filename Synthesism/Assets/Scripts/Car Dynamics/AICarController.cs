2a1f3320daff3778bc08721cb2692f3555abbd54
Mikhail Amod <mickeyamod@gmail.com>
2018-08-07 21:45:08 +0200

    WIP: Added basic braking

--- Synthesism/Assets/Scripts/Car Dynamics/AICarController.cs
+++ Synthesism/Assets/Scripts/Car Dynamics/AICarController.cs
@@ -16,31 +16,120 @@ public class AICarController : MonoBehaviour, ICarController {
     public float maxSpeed = 100f;
     public float maxCornerSpeed = 50f;
 
+    [Header("Sensors")]
+    public float sensorLength = 5f;
+    public Vector3 frontSensorOffset = Vector3.zero;
+    public Vector3 sideSensorOffset = Vector3.zero;
+    public float frontSensorAngle = 30f;
+
     public bool isBraking = false;
 
     public Vector3 centreOfMass;
- 
+
     // Use this for initialization
-    void Start () {
+    void Start() {
         currentNode = 0;
         currentSpeed = 0f;
         initializePath(path);
         GetComponent<Rigidbody>().centerOfMass = centreOfMass;
-	}
-	
-	void FixedUpdate () {
+    }
+
+    void FixedUpdate() {
         MoveVehicle();
         UpdateWaypoint();
     }
 
     public void MoveVehicle()
     {
+        float turnOffset = UseSensors();
         float steerAmount = SteerCar();
         float driveAmount = Drive();
-        carMovement.MoveHorizontal(steerAmount);
+        carMovement.MoveHorizontal(steerAmount*turnOffset);
         carMovement.MoveVertical(driveAmount);
         Brake();
         carMovement.RotateWheels();
+    }
+
+    private float UseSensors()
+    {
+        float turnOffset = 0f;
+        bool oneHit = false;
+        RaycastHit raycastHit;
+        Vector3 frontSensorPos = transform.position;
+        frontSensorPos += transform.forward * frontSensorOffset.z;
+        frontSensorPos += transform.up * frontSensorOffset.y;
+
+        Vector3 frontRightSensorPos = Vector3.Scale(frontSensorPos, new Vector3(1, 1, 1));
+        frontRightSensorPos += transform.right * sideSensorOffset.x;
+
+        Vector3 frontLeftSensorPos = Vector3.Scale(frontSensorPos, new Vector3(1, 1, 1));
+        frontLeftSensorPos -= (transform.right * sideSensorOffset.x);
+
+        //front
+        if (Physics.Raycast(frontSensorPos, transform.forward, out raycastHit, sensorLength))
+        {
+            if (!raycastHit.collider.CompareTag("Track"))
+            {
+                oneHit = true;
+                turnOffset += 0f;
+            }
+            Debug.DrawLine(frontSensorPos, raycastHit.point, Color.red);
+        }
+
+        //front left
+        if (Physics.Raycast(frontLeftSensorPos, transform.forward, out raycastHit, sensorLength))
+        {
+            if (!raycastHit.collider.CompareTag("Track"))
+            {
+                oneHit = true;
+                turnOffset += 1f;
+            }
+            Debug.DrawLine(frontLeftSensorPos, raycastHit.point, Color.red);
+        }
+
+        //front left angled
+        else if (Physics.Raycast(frontLeftSensorPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up)*transform.forward, out raycastHit, sensorLength))
+        {
+            if (!raycastHit.collider.CompareTag("Track"))
+            {
+                oneHit = true;
+                turnOffset += 0.5f;
+            }
+            Debug.DrawLine(frontLeftSensorPos, raycastHit.point, Color.red);
+        }
+
+        //front right
+        if (Physics.Raycast(frontRightSensorPos, transform.forward, out raycastHit, sensorLength))
+        {
+            if (!raycastHit.collider.CompareTag("Track"))
+            {
+                oneHit = true;
+                turnOffset -= 1f;
+            }
+            Debug.DrawLine(frontRightSensorPos, raycastHit.point, Color.red);
+        }
+
+        //front right angled
+        else if (Physics.Raycast(frontRightSensorPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out raycastHit, sensorLength))
+        {
+            if (!raycastHit.collider.CompareTag("Track"))
+            {
+                oneHit = true;
+                turnOffset -= 0.5f;
+            }
+            Debug.DrawLine(frontRightSensorPos, raycastHit.point, Color.red);
+        }
+
+        if (oneHit == true && turnOffset == 0)
+        {
+            isBraking = true;
+            return turnOffset;
+        }
+        else
+        {
+            isBraking = false;
+            return turnOffset;
+        }
     }
 
     private void UpdateWaypoint()
