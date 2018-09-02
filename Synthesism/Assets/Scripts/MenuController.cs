6ad66d05df6ebca70661666dfe81ddaf4cb1f8f1
Mikhail Amod <mickeyamod@gmail.com>
2018-09-02 20:51:02 +0200

    Added MainMenu, MusicManager

---
+++ Synthesism/Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    private void Start()
+    {
+        MusicManager.instance.PlaySong();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
