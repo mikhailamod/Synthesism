2886fc2c1274056d8d36823ed2a01958b9bcd6d2
Mikhail Amod <mickeyamod@gmail.com>
2018-09-04 21:32:32 +0200

    Updated AI, added engine sound

--- Synthesism/Assets/Scripts/Car Dynamics/AICarController.cs
+++ Synthesism/Assets/Scripts/Car Dynamics/AICarController.cs
@@ -21,6 +21,8 @@ public class AICarController : CarController {
     public Vector3 sideSensorOffset = Vector3.zero;
     public float frontSensorAngle = 30f;
 
+    public List<string> listOfTags;
+
     //Private
     private List<Node> nodes;
     private int currentNode;
@@ -72,7 +74,8 @@ public class AICarController : CarController {
         //front left
         if (Physics.Raycast(frontLeftSensorPos, transform.forward, out raycastHit, sensorLength))
         {
-            if (raycastHit.collider.CompareTag("Obstacle"))
+            string theTag = raycastHit.collider.tag;
+            if (listOfTags.Contains(theTag))
             {
                 oneHit = true;
                 turnOffset += 1f;
@@ -83,7 +86,8 @@ public class AICarController : CarController {
         //front left angled
         if (Physics.Raycast(frontLeftSensorPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up)*transform.forward, out raycastHit, sensorLength))
         {
-            if (raycastHit.collider.CompareTag("Obstacle"))
+            string theTag = raycastHit.collider.tag;
+            if (listOfTags.Contains(theTag))
             {
                 oneHit = true;
                 turnOffset += 0.75f;
@@ -95,7 +99,8 @@ public class AICarController : CarController {
         //front right
         if (Physics.Raycast(frontRightSensorPos, transform.forward, out raycastHit, sensorLength))
         {
-            if (raycastHit.collider.CompareTag("Obstacle"))
+            string theTag = raycastHit.collider.tag;
+            if (listOfTags.Contains(theTag))
             {
                 oneHit = true;
                 turnOffset -= 1f;
@@ -107,7 +112,8 @@ public class AICarController : CarController {
         //front right angled
         if (Physics.Raycast(frontRightSensorPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out raycastHit, sensorLength))
         {
-            if (raycastHit.collider.CompareTag("Obstacle"))
+            string theTag = raycastHit.collider.tag;
+            if (listOfTags.Contains(theTag))
             {
                 oneHit = true;
                 turnOffset -= 0.75f;
@@ -121,7 +127,8 @@ public class AICarController : CarController {
             //front
             if (Physics.Raycast(frontSensorPos, transform.forward, out raycastHit, sensorLength))
             {
-                if (raycastHit.collider.CompareTag("Obstacle"))
+                string theTag = raycastHit.collider.tag;
+                if (listOfTags.Contains(theTag))
                 {
                     oneHit = true;
                     if (raycastHit.normal.x < 0) { turnOffset = -1f; }
