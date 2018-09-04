ed49a5865c82202533ba40514553c8507058de24
Pavan Singh <singhpavan78@gmail.com>
2018-09-04 16:59:20 +0200

    Added boost pad

--- Synthesism/Assets/Scripts/Powerups/BoostController.cs
+++ Synthesism/Assets/Scripts/Powerups/BoostController.cs
@@ -3,26 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoostController : MonoBehaviour {
-
-	public float speed = 10f;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		RotateCube();
-
-	}
-
-	void RotateCube() {
-		transform.Rotate(Vector3.up, speed * Time.deltaTime);
-		transform.Rotate(Vector3.left, speed * Time.deltaTime);
-	}
-
 	void OnTriggerEnter(Collider other) {
+		Debug.Log("Collided with boost!");
 		Destroy(this.gameObject);
 		other.gameObject.GetComponent<PlayerCarController>().boost();
 	}
