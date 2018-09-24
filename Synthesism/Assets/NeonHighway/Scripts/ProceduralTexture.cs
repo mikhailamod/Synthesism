b5e09a26aa9b9add80ea0f13d6d2b057d6ecda51
Brandon-Gower-Winter <brandongowerwinter@gmail.com>
2018-09-24 14:18:10 +0200

    Added Color option and tiling option

--- Synthesism/Assets/NeonHighway/Scripts/ProceduralTexture.cs
+++ Synthesism/Assets/NeonHighway/Scripts/ProceduralTexture.cs
@@ -6,18 +6,25 @@ using UnityEngine;
 public class ProceduralTexture : MonoBehaviour {
 
     public Texture2D LightGrid;
+    private Texture2D instanceTexture;
     public Vector2 ImageDimensions;
     public Vector2 WindowDimensions;
 
     public int MaterialID = 0;
+    public Vector2 tiling;
+
+    public Color[] colors;
 
 	void Start ()
     {
         MeshRenderer mr = GetComponent<MeshRenderer>();
         Material light_mat = mr.materials[MaterialID];
+        light_mat.mainTextureScale = tiling;
+        instanceTexture = new Texture2D((int)ImageDimensions.x,(int)ImageDimensions.y);
+        Graphics.CopyTexture(LightGrid, instanceTexture);
+        light_mat.SetTexture("_MainTex",instanceTexture);
         LightUp();
-        light_mat.SetTexture("_MainTex", LightGrid);
-	}
+    }
 
     void LightUp()
     {
@@ -27,19 +34,20 @@ public class ProceduralTexture : MonoBehaviour {
             {
                 if (LightGrid.GetPixel(x, y) == Color.white)
                 {
+                    int randColorID = Random.Range(0, colors.Length);
                     float gradient = Random.Range(0.0f, 1.0f);
-                    Color newColor = Color.Lerp(Color.black, Color.white, gradient);
+                    Color newColor = Color.Lerp(Color.black, colors[randColorID], gradient);
                     for(int i = 0; i < WindowDimensions.y; i++)
                     {
                         for (int j = 0; j < WindowDimensions.x; j++)
                         {
-                            LightGrid.SetPixel(x + j, y + i, newColor);
+                            instanceTexture.SetPixel(x + j, y + i, newColor);
                         }
                     }
                 }
             }
         }
-        LightGrid.Apply();
+        instanceTexture.Apply();
     }
 	
 }
