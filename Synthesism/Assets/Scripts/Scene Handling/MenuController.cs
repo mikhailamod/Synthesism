46ddcd6a165b49ae807d115ebabe4d9a0350cedd
Mikhail Amod <mickeyamod@gmail.com>
2018-10-16 00:18:57 +0200

    Added track chooser

--- Synthesism/Assets/Scripts/Scene Handling/MenuController.cs
+++ Synthesism/Assets/Scripts/Scene Handling/MenuController.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 //Facilitates the use of the main menu
 //A main menu scene should have a MusicManager instance and RaceManager instance
 public class MenuController : MonoBehaviour
 {
 
-    public CarSelector selectController;
-    public GameObject[] children;
+    public TrackSelector trackSelector;
 
     private void Start()
     {
@@ -17,28 +17,24 @@ public class MenuController : MonoBehaviour
     public void SinglePlayer()
     {
         MusicManager.instance.PlaySoundEffect(MusicManagerInfo.MAIN_MENU_BEEP);
-        disableMenu();
-        selectController.setToActive(false);
+        TrackSelector.instance.setMultiplayer(false);
+        TrackSelector.instance.setCanChoose(true);
+        TrackSelector.instance.setTrackChoice(0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void TwoPlayer()
     {
         MusicManager.instance.PlaySoundEffect(MusicManagerInfo.MAIN_MENU_BEEP);
-        disableMenu();
-        selectController.setToActive(true);
+        TrackSelector.instance.setMultiplayer(true);
+        TrackSelector.instance.setCanChoose(true);
+        TrackSelector.instance.setTrackChoice(0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Quit()
     {
         MusicManager.instance.PlaySoundEffect(MusicManagerInfo.MAIN_MENU_BEEP);
         Application.Quit();
-    }
-
-    void disableMenu()
-    {
-        for (int i = 0; i < children.Length; i++)
-        {
-            children[i].SetActive(false);
-        }
     }
 }
